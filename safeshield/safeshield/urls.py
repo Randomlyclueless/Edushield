ef7977c45c5af4c81acb93f303abcb8543e6d738
SAINATH537 <146355746+SAINATH537@users.noreply.github.com>
2025-02-23 00:45:55 +0530

    added

--- safeshield/safeshield/urls.py
+++ safeshield/safeshield/urls.py
@@ -12,3 +12,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),  
     path('verify_otp/', views.verify_otp, name='verify_otp'),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
