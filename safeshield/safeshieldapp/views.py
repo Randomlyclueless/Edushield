e05a7794b5b623e58ee3e03e21559c5d384917f4
Randomlyclueless <144950366+Randomlyclueless@users.noreply.github.com>
2025-02-22 19:11:25 +0530

    signup and login
    signup and login have been created wrt frontend

--- safeshield/safeshieldapp/views.py
+++ safeshield/safeshieldapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def user_login(request):  # Renamed function to avoid conflict
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
